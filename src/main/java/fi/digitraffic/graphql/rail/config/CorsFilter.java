0f66517b0a3c8c2ba42889862e9016d2a90f2e81
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-09-24 10:40:53 +0300

    DPO-1101 cache every request for 5 seconds

---
+++ src/main/java/fi/digitraffic/graphql/rail/config/CorsFilter.java
@@ -0,0 +1,25 @@
+package fi.digitraffic.graphql.rail.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        response.addHeader("Cache-Control", String.format("max-age=%d, public", 5));
+
+        filterChain.doFilter(request, response);
+    }
+
+}
