8be4e6cc06fa00c4f76a16a9b8f47b82596f8901
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-10-06 13:22:21 +0300

    DPO-1101 explicitly disable cache

---
+++ src/main/java/fi/digitraffic/graphql/rail/config/CacheControlFilter.java
@@ -0,0 +1,25 @@
+package fi.digitraffic.graphql.rail.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class CacheControlFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        response.addHeader("Cache-Control", "Cache-Control: no-cache");
+
+        filterChain.doFilter(request, response);
+    }
+
+}
