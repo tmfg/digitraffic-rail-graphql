792f46820309bb00a446784ee710fe5e2777abab
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-12-04 15:14:22 +0200

    Cors

---
+++ src/main/java/fi/digitraffic/graphql/rail/config/CorsFilter.java
@@ -0,0 +1,39 @@
+package fi.digitraffic.graphql.rail.config;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        final HttpServletRequest request = (HttpServletRequest) req;
+
+        //spring-websocket manages these
+        if (!request.getRequestURI().contains("/websockets/")) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+        }
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
