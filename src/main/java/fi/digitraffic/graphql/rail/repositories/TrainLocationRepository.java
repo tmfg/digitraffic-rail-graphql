427b01f32c60cb7c4e82a36fd0312e692aacbc1f
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2024-03-15 10:36:00 +0200

    Fixed compilation errors

--- src/main/java/fi/digitraffic/graphql/rail/repositories/TrainLocationRepository.java
+++ src/main/java/fi/digitraffic/graphql/rail/repositories/TrainLocationRepository.java
@@ -1,6 +1,10 @@
 package fi.digitraffic.graphql.rail.repositories;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,4 +13,9 @@ import fi.digitraffic.graphql.rail.entities.TrainLocation;
 @Repository
 @Transactional
 public interface TrainLocationRepository extends JpaRepository<TrainLocation, Long> {
+    @Query("select max(tl.id) " +
+        "   from TrainLocation tl " +
+        "   where tl.trainLocationId.timestamp >= ?1 " +
+        "   group by tl.trainLocationId.departureDate,tl.trainLocationId.trainNumber")
+    List<Long> findLatest(ZonedDateTime timestampAfter);
 }
--- src/main/java/fi/digitraffic/graphql/rail/repositories/TrainRepository.java
+++ src/main/java/fi/digitraffic/graphql/rail/repositories/TrainRepository.java
@@ -1,6 +1,9 @@
 package fi.digitraffic.graphql.rail.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +13,55 @@ import fi.digitraffic.graphql.rail.entities.TrainId;
 @Repository
 @Transactional
 public interface TrainRepository extends JpaRepository<Train, TrainId> {
+    @Query(value = "select * from ((SELECT " +
+        "    '1', t.departure_date, t.train_number, t.version" +
+        " FROM" +
+        "    live_time_table_train t" +
+        " WHERE" +
+        "   (t.train_stopping = true or t.train_stopping = ?6)" +
+        "   AND (?1 is null OR t.station_short_code = ?1)" +
+        "   AND t.type = '1'" +
+        "   AND (t.train_category_id in ?7)" +
+        "   AND t.actual_time IS NOT NULL" +
+        "   AND (t.deleted IS NULL OR t.deleted = 0)" +
+        " ORDER BY t.actual_time DESC" +
+        " LIMIT ?2) UNION ALL (SELECT " +
+        "    '2', t.departure_date, t.train_number, t.version" +
+        " FROM" +
+        "    live_time_table_train t" +
+        " WHERE" +
+        "   (t.train_stopping = true or t.train_stopping = ?6)" +
+        "   AND (?1 is null OR t.station_short_code = ?1)" +
+        "   AND t.type = '1'" +
+        "   AND (t.train_category_id in ?7)" +
+        "   AND t.actual_time IS NULL" +
+        "   AND (t.deleted IS NULL OR t.deleted = 0)" +
+        " ORDER BY t.predict_time ASC" +
+        " LIMIT ?3) UNION ALL (SELECT " +
+        "    '3', t.departure_date, t.train_number, t.version" +
+        " FROM" +
+        "    live_time_table_train t" +
+        " WHERE" +
+        "   (t.train_stopping = true or t.train_stopping = ?6)" +
+        "   AND (?1 is null OR t.station_short_code = ?1)" +
+        "   AND t.type = '0'" +
+        "   AND (t.train_category_id in ?7)" +
+        "   AND t.actual_time IS NOT NULL" +
+        "   AND (t.deleted IS NULL OR t.deleted = 0)" +
+        " ORDER BY t.actual_time DESC" +
+        " LIMIT ?4) UNION ALL (SELECT " +
+        "    '4', t.departure_date, t.train_number, t.version" +
+        " FROM" +
+        "    live_time_table_train t" +
+        " WHERE" +
+        "   (t.train_stopping = true or t.train_stopping = ?6)" +
+        "   AND (?1 is null OR t.station_short_code = ?1)" +
+        "   AND t.type = '0'" +
+        "   AND (t.train_category_id in ?7)" +
+        "   AND t.actual_time IS NULL" +
+        "   AND (t.deleted IS NULL OR t.deleted = 0)" +
+        " ORDER BY t.predict_time ASC" +
+        " LIMIT ?5)) unionedTable", nativeQuery = true)
+    List<Object[]> findLiveTrainsIds(final String station, final Integer departedTrains, final Integer departingTrains, final Integer arrivedTrains,
+                                     final Integer arrivingTrains, final Boolean excludeNonstopping, final List<Long> trainCategoryIds);
 }
