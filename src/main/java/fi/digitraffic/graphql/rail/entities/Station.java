92b3a045bd3411be4a4a50c83214e0ffe9a2017a
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2024-03-15 09:35:54 +0200

    Removed unnecessary annotations

--- src/main/java/fi/digitraffic/graphql/rail/entities/Station.java
+++ src/main/java/fi/digitraffic/graphql/rail/entities/Station.java
@@ -1,16 +1,12 @@
 package fi.digitraffic.graphql.rail.entities;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 public class Station {
-    @JsonProperty("stationName")
     public String name;
-    @JsonProperty("stationShortCode")
     public String shortCode;
-    @JsonProperty("stationUICCode")
     public int uicCode;
     public String countryCode;
     public Double longitude;
