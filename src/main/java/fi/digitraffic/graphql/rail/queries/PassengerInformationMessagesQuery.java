9c93a2ca7fc664bd272316dc1ca580eedadcd617
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-05-15 16:04:44 +0300

    DPO-2534 Only return latest version

--- src/main/java/fi/digitraffic/graphql/rail/queries/PassengerInformationMessagesQuery.java
+++ src/main/java/fi/digitraffic/graphql/rail/queries/PassengerInformationMessagesQuery.java
@@ -13,6 +13,7 @@ import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 
 import fi.digitraffic.graphql.rail.entities.PassengerInformationMessage;
@@ -51,8 +52,9 @@ public class PassengerInformationMessagesQuery extends BaseQuery<PassengerInform
 
     @Override
     public BooleanExpression createWhereFromArguments(final DataFetchingEnvironment dataFetchingEnvironment) {
-        return QPassengerInformationMessage.passengerInformationMessage.endValidity.after(ZonedDateTime.now()).and(
-                QPassengerInformationMessage.passengerInformationMessage.startValidity.before(ZonedDateTime.now()));
+        return QPassengerInformationMessage.passengerInformationMessage.startValidity.before(ZonedDateTime.now())
+                .and(QPassengerInformationMessage.passengerInformationMessage.endValidity.after(ZonedDateTime.now()));
+
     }
 
     @Override
@@ -70,11 +72,19 @@ public class PassengerInformationMessagesQuery extends BaseQuery<PassengerInform
         final Expression<?>[] allFields = Stream.of(
                 QPassengerInformationMessage.passengerInformationMessage).toArray(Expression<?>[]::new);
 
+        final JPAQuery<Tuple> maxVersions = super.queryFactory.select(
+                        QPassengerInformationMessage.passengerInformationMessage.id,
+                        QPassengerInformationMessage.passengerInformationMessage.version.max())
+                .from(QPassengerInformationMessage.passengerInformationMessage)
+                .groupBy(QPassengerInformationMessage.passengerInformationMessage.id);
+
         final JPAQuery<Tuple> queryAfterFrom = super.queryFactory.selectDistinct(allFields)
                 .from(getEntityTable())
                 .leftJoin(QPassengerInformationMessage.passengerInformationMessage.audio).fetchJoin()
                 .leftJoin(QPassengerInformationMessage.passengerInformationMessage.video).fetchJoin()
-                .leftJoin(QPassengerInformationMessage.passengerInformationMessage.train).fetchJoin();
+                .leftJoin(QPassengerInformationMessage.passengerInformationMessage.train).fetchJoin()
+                .where(Expressions.list(QPassengerInformationMessage.passengerInformationMessage.id,
+                        QPassengerInformationMessage.passengerInformationMessage.version).in(maxVersions));
 
         return super.createFetcher(queryAfterFrom);
     }
